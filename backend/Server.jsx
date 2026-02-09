aecf88fd862b69cbad9cf5de707c504081633fc9
legha <leghasri162006@gmail.com>
2026-02-09 10:32:13 +0530

    first commit

---
+++ backend/Server.jsx
@@ -0,0 +1,46 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+// DB connection
+mongoose.connect("mongodb://localhost:27017/vcetstudents")
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+
+const StudentSchema = new mongoose.Schema({
+  name: String,
+  course: String
+});
+
+const Student = mongoose.model("Student", StudentSchema);
+
+// CREATE
+app.post("/add", async (req, res) => {
+  const student = new Student(req.body);
+  await student.save();
+  res.send("Student Added");
+});
+
+// READ
+app.get("/students", async (req, res) => {
+  const data = await Student.find();
+  res.json(data);
+});
+
+// DELETE
+app.delete("/delete/:id", async (req, res) => {
+  await Student.findByIdAndDelete(req.params.id);
+  res.send("Student Deleted");
+});
+
+// server start
+app.listen(5000, () => {
+  console.log("Backend running on port 5000");
+});
